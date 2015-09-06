dd4c7b8c9e47e16a63865d08e961f4325f6a6e0d
Alec Kretch <aleckretch@Alecs-MacBook-Air.local>
2015-09-05 20:02:06 -0700

    podspec added

---
+++ AKImageViewerViewController.h
@@ -0,0 +1,23 @@
+//
+//  AKImageViewerViewController.h
+//  AKImageViewerViewController
+//
+//  Created by Alec Kretch on 8/28/15.
+//  Copyright (c) 2015 Alec Kretch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AKImageViewerViewController : UIViewController <UIActionSheetDelegate, UIScrollViewDelegate>
+
+@property (retain, nonatomic) IBOutlet UIButton *btnCancel;
+@property (retain, nonatomic) IBOutlet UIImage *image;
+@property (retain, nonatomic) IBOutlet UIImageView *imageView;
+@property (retain, nonatomic) IBOutlet UIPanGestureRecognizer *pan;
+@property (retain, nonatomic) IBOutlet UIScrollView *scrollView;
+
+@property (assign, nonatomic) BOOL disableSavingImage;
+
+- (void)centerPictureFromPoint:(CGPoint)point ofSize:(CGSize)size withCornerRadius:(float)radius;
+
+@end
